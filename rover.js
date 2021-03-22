0b59427887298ed4dc94148308c944fccb2ec71c
James Chambers <chambersjamesd@gmail.com>
2021-03-22 03:38:48 +0000

    Stuck.

--- rover.js
+++ rover.js
@@ -1,5 +1,15 @@
 class Rover {
-   // Write code here!
+   constructor(position) {
+     this.position = position;
+     this.mode = 'NORMAL'
+     this.generatorWatts = 110
+   }
+   receiveMessage(message){
+    this.message = message['name'];
+    this.commands = message['commands'];
+    this.results = {}
+    return(this);
+  }
 }
 
-module.exports = Rover;
+module.exports = Rover, Rover.receiveMessage;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,30 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts",
+  function () {
+    let rover = new Rover(1234);
+    expect(rover.mode).toEqual('NORMAL');
+    expect(rover.generatorWatts).toEqual(110);
+  })
+
+  it("response returned by receiveMessage contains name of message", function () {
+    let rover = new Rover(1234);
+    let message = new Message('Test Message');
+    let response = rover.receiveMessage(message);
+    expect(response.message).toEqual('Test Message');
+  });
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function () {
+    let rover = new Rover(1234);
+    let commands = ['command 1', 'command 2'];
+    let message = new Message('Test Message', commands);
+    let response = rover.receiveMessage(message);
+    expect(response.commands).toEqual(commands);
+  });
+
+  it("responds correctly to status check command", function () {
+    
+  });
 
 });
